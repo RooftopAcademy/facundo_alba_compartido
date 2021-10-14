dc3d511c5e8752ab43931d4188206b8b90768ff5
FacundoAlba <69168381+FacundoAlba@users.noreply.github.com>
2021-10-14 17:02:14 -0300

    Update Store.js

--- src/javascript/Store.js
+++ src/javascript/Store.js
@@ -1,7 +1,46 @@
 
-class Store{
-    constructor(){
-        this.name = String
-        this.id = Number
+const cards = document.getElementById("cards")
+const templateCard = document.getElementById("template-card").content
+const fragment = document.createDocumentFragment()
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetchData()
+
+})
+
+const fetchData = async() => {
+    try{
+        const res= await fetch("../../store.json")
+        const data = await res.json()
+        addStores(data)
+        
+    } catch(error){
+        console.log(error)
     }
 }
+
+const addStores= data =>{
+    data.forEach(store => {
+        templateCard.querySelector('h3').textContent = store.storeName
+        templateCard.querySelector("h5").textContent = store.description
+        templateCard.getElementById("img").setAttribute("src", store.urlImage)
+        templateCard.getElementById("img-logo").setAttribute("src", store.urlLogo)
+        
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cards.appendChild(fragment)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
